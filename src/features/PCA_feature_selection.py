4553db70df2139f2cebfa594f0e8f9aacabc3415
Jonny Tran <nhat.tran@mavs.uta.edu>
2019-11-10 12:29:45 -0600

    added PCA feature selction transformer model

--- src/features/PCA_feature_selection.py
+++ src/features/PCA_feature_selection.py
@@ -10,14 +10,14 @@ class PCA_Variants2Gene_FeatureSelection(sklearn.base.TransformerMixin):
         self.variants_top56_genes_path = variants_genes_path
         self.variance_threshold = variance_threshold
 
+        self.variants_top56_genes = pd.read_table(self.variants_top56_genes_path, sep=",")
+        self.variants_top56_genes.set_index("Variant ID", inplace=True)
+        self.variants_top56_genes = self.variants_top56_genes.filter(items=["Gene(s)", "Variant ID"])
         super().__init__()
 
     def get_mutations_by_gene_dict(self, snps_df):
-        variants_top56_genes = pd.read_table(self.variants_top56_genes_path, sep=",")
-        variants_top56_genes.set_index("Variant ID", inplace=True)
-        variants_top56_genes = variants_top56_genes.filter(items=["Gene(s)", "Variant ID"])
-        # genes_matched_variants = snps_df.columns & variants_top56_genes.index
-        mutations_gene_matched = snps_df.T.join(variants_top56_genes, how="right")
+        # genes_matched_variants = snps_df.columns & self.variants_top56_genes.index
+        mutations_gene_matched = snps_df.T.join(self.variants_top56_genes, how="right")
         mutations_by_gene = {}
         mutations_gene_gb = mutations_gene_matched.groupby("Gene(s)")
 
@@ -31,7 +31,7 @@ class PCA_Variants2Gene_FeatureSelection(sklearn.base.TransformerMixin):
         return mutations_by_gene
 
 
-    def fit_transform(self, X, y=None, **fit_params):
+    def fit_transform(self, X, y=None, verbose=False):
         """
 
         :param X: SNPs DataFrame with sample id rows and SNP site columns (i.e. chromesome:location)
@@ -41,6 +41,7 @@ class PCA_Variants2Gene_FeatureSelection(sklearn.base.TransformerMixin):
         mutations_by_gene = self.get_mutations_by_gene_dict(X)
         self.PCA_by_gene = {}
         self.top_k_PC_by_gene = {}
+        self.genes = mutations_by_gene.keys()
 
         # FIT PCAs
         for gene in mutations_by_gene.keys():
@@ -49,7 +50,7 @@ class PCA_Variants2Gene_FeatureSelection(sklearn.base.TransformerMixin):
             top_k = np.argmax(np.cumsum(np.power(self.PCA_by_gene[gene].singular_values_, 2)) /
                               np.sum(np.power(self.PCA_by_gene[gene].singular_values_, 2)) > self.variance_threshold)
             self.top_k_PC_by_gene[gene] = top_k
-            print(gene, "top_k", top_k, mutations_by_gene[gene].shape)
+            print(gene, "top_k", top_k, mutations_by_gene[gene].shape) if verbose else None
 
         # Transform X to top_k pca loadings by gene
         x_transform_list = []
@@ -58,7 +59,7 @@ class PCA_Variants2Gene_FeatureSelection(sklearn.base.TransformerMixin):
             x_transform_list.append(x_by_gene_transform[:, :self.top_k_PC_by_gene[gene]])
 
         pca_projs_concat = np.concatenate(x_transform_list,axis=1)
-        print("pca_projs_concat.shape", pca_projs_concat.shape)
+        print("pca_projs_concat.shape", pca_projs_concat.shape) if verbose else None
 
         pca_proj_names = []
         for i, gene in enumerate(mutations_by_gene.keys()):
@@ -85,28 +86,25 @@ class PCA_Variants2Gene_FeatureSelection(sklearn.base.TransformerMixin):
 
         return pd.DataFrame(pca_projs_concat, index=X.index, columns=pca_proj_names)
 
+    def get_gene_variability_score(self, X):
+        X_1 = self.transform(X)
+        patient_gene_proj_scores = pd.DataFrame(index=X_1.index, columns=self.genes)
+        patient_gene_proj_scores.index.name = "patient_id"
 
-def get_top_k_components(snp_data, var_threshold=0.80, return_fit_transform=False):
-    pca = PCA()
-    pca.fit(snp_data)
-    top_k = np.argmax(np.cumsum(np.power(pca.singular_values_, 2)) /
-                      np.sum(np.power(pca.singular_values_, 2)) > var_threshold)
+        for gene in self.genes:
+            gene_projs = X_1.iloc[:, X_1.columns.str.contains(gene)]
+            patient_gene_proj_scores[gene] = np.power(gene_projs, 2).sum(axis=1) / gene_projs.columns.shape[0]
 
-    if return_fit_transform:
-        pca.n_components = top_k
-        pca.n_components_ = top_k
-        return pca.transform(snp_data)[:, :top_k]
-    else:
-        return pca.components_[:top_k]
+        return patient_gene_proj_scores
+
 
 def get_top_variant_by_coef(pca_components, coef_percentile=70):
     gene_coefs_sum = np.abs(pca_components).sum(axis=0)
     coefs_percentile = np.percentile(gene_coefs_sum, coef_percentile)
     return np.where(gene_coefs_sum > coefs_percentile)
 
-def select_top_variants(snp_data, var_threshold=0.80, coef_percentile=70):
+def select_top_variants(snp_data, top_k_components, coef_percentile=70):
     variants = snp_data.columns
-    top_k_components = get_top_k_components(snp_data, var_threshold)
     top_variants_idx = get_top_variant_by_coef(top_k_components, coef_percentile)
 
     return variants[top_variants_idx]
